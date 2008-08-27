b8a9319ef9b421d49dc0efee1ca4884a7eec9881
Kathleen Bonnell <bonnell2@llnl.gov>
2008-08-27 22:17:31 +0000

    Fix streamline atts initializer to prevent segv on windows platform
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/1.10RC/src@5081 18c085ea-50e0-402c-830e-de6fd14e8384

--- plots/Streamline/avtStreamlineFilter.C
+++ plots/Streamline/avtStreamlineFilter.C
@@ -460,6 +460,9 @@ avtStreamlineWrapper::ComputeStatistics()
 //   Dave Pugmire, Tue Aug 19 17:13:04EST 2008
 //   Remove accurate distance calculate option.
 //
+//   Kathleen Bonnell, Wed Aug 27 15:13:07 PDT 2008
+//   Initialize solver.
+//
 // ****************************************************************************
 
 avtStreamlineFilter::avtStreamlineFilter()
@@ -479,6 +482,7 @@ avtStreamlineFilter::avtStreamlineFilter()
     absTol = 0;
     haveGhostZones = false;
     intervalTree = NULL;
+    solver = NULL;
     dataSpatialDimension = 3;
 
     //
