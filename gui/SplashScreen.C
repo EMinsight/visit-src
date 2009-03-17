76ccdb64667a5b8233f038f99d54bcc0a04ec285
Eric Brugger <brugger1@llnl.gov>
2009-03-17 22:39:17 +0000

    1) I corrected an error in avtGenericDatabase that prevented ghost zones    from being added to rectilinear meshes for readers that did their own    domain decomposition.
    2) I updated the 1.11.2 release notes with all the changes made for 1.11.2
       up until now. This was over a dozen changes.
    
    3) I updated the date on the splash screen
    
    
    
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/1.11RC/src@6537 18c085ea-50e0-402c-830e-de6fd14e8384

--- gui/SplashScreen.C
+++ gui/SplashScreen.C
@@ -141,6 +141,9 @@
 //    Eric Brugger, Mon Dec 29 15:08:57 PST 2008
 //    Changed the date on the splash screen.
 //
+//    Eric Brugger, Tue Mar 17 15:36:27 PDT 2009
+//    Changed the date on the splash screen.
+//
 // ****************************************************************************
 
 SplashScreen::SplashScreen(bool cyclePictures, const char *name) :
@@ -247,7 +250,7 @@ SplashScreen::SplashScreen(bool cyclePictures, const char *name) :
     versionText += " ";
     versionText += SVN_REVISION;
     lLayout->addWidget(new QLabel(versionText, this, "versionText"));
-    lLayout->addWidget(new QLabel(tr("January 2009"), this, "dateCompiled"));
+    lLayout->addWidget(new QLabel(tr("March 2009"), this, "dateCompiled"));
 
     copyrightButton = 0;
     contributorButton = 0;
