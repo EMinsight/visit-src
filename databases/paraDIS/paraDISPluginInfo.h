383e229216616d9cad2c632a073316852089ff0c
Rich Cook <rcook@llnl.gov>
2010-03-17 21:41:58 +0000

    use new heuristics and GUI elements to allow "threshold" values to be selected for loop searching
    git-svn-id: svn+ssh://edison.nersc.gov/project/projectdirs/visit/svn/visit/branches/1.12RC/src@10382 18c085ea-50e0-402c-830e-de6fd14e8384

--- databases/paraDIS/paraDISPluginInfo.h
+++ databases/paraDIS/paraDISPluginInfo.h
@@ -80,6 +80,8 @@ class paraDISCommonPluginInfo : public virtual CommonDatabasePluginInfo, public 
     virtual DatabaseType              GetDatabaseType();
     virtual avtDatabase              *SetupDatabase(const char * const *list,
                                                     int nList, int nBlock);
+    virtual DBOptionsAttributes *GetReadOptions() const;
+    virtual DBOptionsAttributes *GetWriteOptions() const;
 };
 
 class paraDISMDServerPluginInfo : public virtual MDServerDatabasePluginInfo, public virtual paraDISCommonPluginInfo
