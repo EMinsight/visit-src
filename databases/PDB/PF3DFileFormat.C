fab8d65a46d65753f4f36b639409a25d5d5b9181
Cyrus Harrison <cyrush@llnl.gov>
2018-06-19 15:25:29 -0700

    2.10.2 release

--- databases/PDB/PF3DFileFormat.C
+++ databases/PDB/PF3DFileFormat.C
@@ -1027,6 +1027,11 @@ PF3DFileFormat::FreeUpResources()
 //   Brad Whitlock, Thu Sep 7 16:28:34 PST 2006
 //   Added more debugging log information.
 //
+//   Eric Brugger, Tue Mar 29 08:22:09 PDT 2016
+//   Temporarily disabled setting up domain connectivity since it is a
+//   memory hog and we don't need them for volume rendering, which is the
+//   primary visualization technique used with PF3D data.
+//
 // ****************************************************************************
 
 void
@@ -1174,12 +1179,20 @@ PF3DFileFormat::PopulateDatabaseMetaData(avtDatabaseMetaData *md)
         debug4 << "glob_nams and int_nams are not the same size!" << endl;
     }
 
+#if 0
     //
     // Set up the domain connectivity arrays so this file format can
     // use VisIt's automatic ghost zone creation facilities.
     //
     debug4 << mName << "Setting up domain connectivity." << endl;
     SetUpDomainConnectivity();
+#else
+    //
+    // Temporarily skip setting up domain connectivity since it is a
+    // memory hog.
+    //
+    debug4 << mName << "Skipping setting up domain connectivity." << endl;
+#endif
 
     debug4 << mName << "end" << endl;
 }
@@ -2368,6 +2381,9 @@ PF3DFileFormat::MasterInformation::GetNDomains() const
 //   Eric Brugger, Fri Oct 30 08:44:02 PDT 2015
 //   Added support for reading multi level files.
 //   
+//   Eric Brugger, Fri Mar  4 14:39:00 PST 2016
+//   I corrected a bug reading the multi level files.
+//
 // ****************************************************************************
 
 bool
@@ -2799,7 +2815,7 @@ PF3DFileFormat::MasterInformation::Read(PDBFileObject *pdb, int cycle,
                 strncpy(viz_nams_new+nd, vizdir, viz_nams_len);
 
                 // The second value is the filename.
-                long ifile = iChunk * nfiles_per_dir + id / nhostgroup_leaders;
+                long ifile = iChunk * nfiles_per_dir + id / (ngrp_members / nfiles_per_dir);
                 sprintf(vizfile, "%sp%dvs%d.pdb", basename, ifile, cycle);
                 strncpy(viz_nams_new+nd+viz_nams_len, vizfile, viz_nams_len);
             }
